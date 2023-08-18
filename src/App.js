bedb79c669fe63957a602fa8ddeb12264120750b
akash-sharma12 <akashsharma746594@gmail.com>
2023-08-18 13:21:47 +0530

    create main navbar and its components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>This is resume builder website</h1>
-        <p>hello one </p>
-      </header>
+     
+        <Navbar/>
     </div>
   );
 }
