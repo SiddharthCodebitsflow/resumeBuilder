9d8d2d5b80d48975638ae9bd0f39567a2651ea28
akashm06 <iakashmaddhehsiya@gmail.com>
2023-08-19 11:15:42 +0530

    make footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
      
         <Navbar/>
+        <Footer/>
     </div>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Hello akahs mad</h1>
+    <div className="footer">
+      <div>
+        <h3>Codebitsflow Software Solutions Pvt. Ltd. </h3>
+        <p>
+          Codebitsflow Software solutions is a software development company that
+          specializes in creating cutting-edge solutions for a wide range of
+          industries. Founded in 2023
+        </p>
+      </div>
+      <div>
+        <h3>Codebitsflow Software Solutions Pvt. Ltd. </h3>
+        <p>
+          Codebitsflow Software solutions is a software development company that
+          specializes in creating cutting-edge solutions for a wide range of
+          industries. Founded in 2023
+        </p>
+      </div>
+      <div>
+        <h3>Codebitsflow Software Solutions Pvt. Ltd. </h3>
+        <p>
+          Codebitsflow Software solutions is a software development company that
+          specializes in creating cutting-edge solutions for a wide range of
+          industries. Founded in 2023
+        </p>
+      </div>
+      <div>
+        <h3>Codebitsflow Software Solutions Pvt. Ltd. </h3>
+        <p>
+          Codebitsflow Software solutions is a software development company that
+          specializes in creating cutting-edge solutions for a wide range of
+          industries. Founded in 2023
+        </p>
+      </div>
     </div>
-  )
+  );
 }
