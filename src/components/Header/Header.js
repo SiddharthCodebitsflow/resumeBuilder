78d90d662b62e41d42f17b3c1e54661287854fb7
akash-sharma12 <akashsharma746594@gmail.com>
2023-08-23 00:16:28 +0530

    craete hamburger and make responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-      <div className="nav-section">
-        <div className='logo'>
-          <img src="logo-2.png" alt="" />
-        </div>
-        <div className="menu">
-          <a>Resume Template</a>
-          <a>CV Templates</a>
-          <a>Cover Letters</a>
-          <a>Plans and pricing</a>
-          <a>Learn</a>
+    <div className="nav-section">
+      <div className='logo'>
+        <a href="#"><img src="logo-2.png" alt="" /></a>
+      </div>
+      <input type='checkbox' id='check' />
+      <div className="menu">
+        <a href="">Resume Template</a>
+        <a href="">CV Templates</a>
+        <a href="">Cover Letters</a>
+        <a href="">Plans and pricing</a>
+        <a href="">Learn</a>
         <button className='nav-text'>Register</button>
         <button className='nav-text'>Sign In</button>
-        </div>
+        <label htmlFor="check" className="close-menu"><FontAwesomeIcon icon={faXmark} className="xmark-icon"/></label>
       </div>
+      <label htmlFor="check" className="open-menu"><FontAwesomeIcon icon={faBars} /></label>
+    </div>
   )
 }
 
 export default Header;
-
